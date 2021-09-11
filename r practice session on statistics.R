0e4ebf9ff346f09f6b29663670f4968101283376
Mayur Kharivle <mayurkharivle59@gmail.com>
2021-09-12 01:14:09 +0530

    Add files via upload
    r practice session on statistics

---
+++ r practice session on statistics.R
@@ -0,0 +1,14 @@
+eans<-datasets::anscombe
+head(eans,1,2)
+tail(eans,1,2)
+summary(eans)
+summary(eans$x1)
+summary(eans$y4)
+plot(eans$x1)
+barplot(eans$y1,xlab = 'summary')
+boxplot(eans$y1,type='l')
+plot(eans$y2,type = 'p')
+sd(eans$y4
+skewness(eans$x1
+plot(eans$x1)
+     
