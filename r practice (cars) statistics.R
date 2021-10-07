349351ff925562ba9e6e65f878366f03635fca40
Mayur Kharivle <mayurkharivle59@gmail.com>
2021-10-07 15:17:31 +0530

    Add files via upload

--- r practice (cars) statistics.R
+++ r practice (cars) statistics.R
@@ -1,18 +1,11 @@
-ecars<-datasets::cars
-head(ecars,5)
-tail(ecars,5)
-ecars[,c(1,2)]
-df<-ecars[,2]
+evolcano<-datasets::volcano
+head(evolcano,5)
+tail(evolcano,5)
+evolcano[,c(1,2)]
+df<-evolcano[,2]
 ecars$speed
-summary(ecars$speed)
-summary(ecars)
-summary(ecars$dist)
-plot(ecars$speed)
-plot(ecars$speed,ecars$dist,type="p")
-plot(ecars)
-plot(ecars$speed,type = "l")
-plot(ecars$dist,xlab = 'Distance Covered')
-barplot(ecars$speed,ecars$dist,main = 'speed')
-hist(ecars$speed)
-hist(ecars$dist)
-hist(ecars$speed)
+summary(evolcano,v1)
+summary(evolcano)
+plot(evolcano)
+barplot(evolcano)
+hist(evolcano)
